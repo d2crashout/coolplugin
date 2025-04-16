8de83f2229d8a413bfcc6cf692d6ee1db6eedb01
jaguarkid817 <methebestestofall@gmail.com>
2025-04-16 07:52:21 -0400

    initial commit

---
+++ src/main/java/org/pluginmakers/firstplugin.java
@@ -0,0 +1,31 @@
+package org.pluginmakers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class firstplugin extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        // 1.
+        // This method will be called on load. NOT ENABLE
+
+        getLogger().info("FirstPlugin has loaded!");
+    }
+
+    @Override
+    public void onEnable() {
+        // This method will be called on plugin enable
+        // 2.
+        getLogger().info("Your first plugin has been enabled!");
+
+        getLogger().severe("Test error message. Its all okay.");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        // 3.
+        // This method will be called on server shutdown
+        getLogger().info("Your first plugin is being disabled!");
+    }
+}
